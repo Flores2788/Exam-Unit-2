3452260a8d508cebc00f309568ebc7c23b05495c
Giorgio Fabbroni <giorgiofabbroni@hotmail.com>
2025-02-22 11:33:21 +0100

    added function that calls the traverse function on the left and right children of the current node

--- Task_3.js
+++ Task_3.js
@@ -1,4 +1,8 @@
 function traverse(node, level = 0){
     if (!node) return { sum: 0, depth: level, count: 0 };
+
+    const left = traverse(node.left, level + 1);
+    const right = traverse(node.right, level + 1);
+
     
 }
