cdfc705128dfbf3cd4e6d893e3f21316844eeca0
Giorgio Fabbroni <giorgiofabbroni@hotmail.com>
2025-02-22 11:41:09 +0100

    added sum of node value and sums of left and right subtrees, max depth, total number of nodes, and return

--- Task_3.js
+++ Task_3.js
@@ -4,5 +4,9 @@ function traverse(node, level = 0){
     const left = traverse(node.left, level + 1);
     const right = traverse(node.right, level + 1);
 
-    
+    const sum = node.value + left.sum + right.sum;
+    const depth = Math.max(left.depth, right.depth);
+    const count = 1 + left.count + right.count;
+
+    return { sum, depth, count };
 }
